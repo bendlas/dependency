e832d16dcb467cfe2d36aca2dfe0676ab06970e9
Herwig Hochleitner <herwig@bendlas.net>
2019-03-22 17:19:49 +0100

    valutico deployment

--- project.clj
+++ project.clj
@@ -1,7 +1,9 @@
-(defproject com.stuartsierra/dependency "0.2.1-SNAPSHOT"
+(defproject com.stuartsierra/dependency "0.2.0-VALUTICO1"
   :description "A data structure for representing dependency graphs"
   :url "https://github.com/stuartsierra/dependency"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :profiles {:dev {:dependencies [[org.clojure/clojure "1.8.0-alpha5"]]}
-             :clj-1.7.0 {:dependencies [[org.clojure/clojure "1.7.0"]]}})
+  :profiles {:dev {:dependencies [[org.clojure/clojure "1.10.0"]]}
+             :clj-1.7.0 {:dependencies [[org.clojure/clojure "1.7.0"]]}}
+  :repositories [["valutico-snapshots" {:url "https://nexus.valutico.com/repository/maven-snapshots" :creds :gpg}]
+                 ["valutico-releases" {:url "https://nexus.valutico.com/repository/maven-releases" :creds :gpg}]])
